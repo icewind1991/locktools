02472a99aa8632d20f6dff411b75fb06cf5f1be4
Robin Appelman <icewind@owncloud.com>
2015-09-18 23:44:57 +0200

    inifite scroll

--- js/Components/LockLog.js
+++ js/Components/LockLog.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react/addons';
 import Timestamp from 'react-time';
+import ReactList from 'react-list';
 
 import {LockType} from './LockType';
 import {LockState} from './LockState'
@@ -24,58 +25,57 @@ export class LockLog extends Component {
 		this.setState({showStates});
 	}
 
-	render () {
-		const rows = this.state.entries.map(entry => {
-			const onClick = (entry.event === 'error') ? function () {
-			} : this.toggleShowState.bind(this, entry.key);
-			const body = (this.state.showStates[entry.key])
-				? (<LockState
-				state={this.props.provider.calculateState(this.state.entries, entry.key)}/>)
-				: '';
-			const className = (entry.event === 'error') ? style.error : style.event;
-			const event = (entry.event === 'error') ? 'Error on ' + entry.params.operation : entry.event;
-			return (
-				<tr key={entry.key} className={className}
-					onClick={onClick}>
-					<td className={style.time}><Timestamp
-						value={entry.time * 1000}
-						relative
-						titleFormat="HH:mm:ss.SSS"/>
-					</td>
-					<td className={style.event}>{event}</td>
-					<td className={style.path}>{entry.path} {body}</td>
-					<td className={style.type}>
-						<LockType type={entry.params.type}/>
-					</td>
-				</tr>
-			)
-		});
-		if (rows.length === 0) {
-			rows.push((
-				<tr className={style.empty}>
-					<td colSpan={4}>No log entries found</td>
-				</tr>
-			));
-		}
-		//rows.unshift((
-		//	<tr className={style.new}>
-		//		<td colSpan={4}>New lock</td>
-		//	</tr>
-		//))
+	renderRow = (index, key) => {
+		const entry = this.state.entries[index];
+		const onClick = (entry.event === 'error') ? function () {
+		} : this.toggleShowState.bind(this, entry.key);
+		const body = (this.state.showStates[entry.key])
+			? (<LockState
+			state={this.props.provider.calculateState(this.state.entries, entry.key)}/>)
+			: '';
+		const className = (entry.event === 'error') ? style.error : style.event;
+		const event = (entry.event === 'error') ? 'Error on ' + entry.params.operation : entry.event;
 		return (
-			<table className={style.locklog}>
-				<thead>
-				<tr>
-					<th className={style.time}>Time</th>
-					<th className={style.event}>Event</th>
-					<th className={style.path}>Path</th>
-					<th className={style.type}>Type</th>
-				</tr>
-				</thead>
-				<tbody>
-				{rows}
-				</tbody>
-			</table>
+			<tr key={entry.key} className={className}
+				onClick={onClick}>
+				<td className={style.time}><Timestamp
+					value={entry.time * 1000}
+					relative
+					titleFormat="HH:mm:ss.SSS"/>
+				</td>
+				<td className={style.event}>{event}</td>
+				<td className={style.path}>{entry.path} {body}</td>
+				<td className={style.type}>
+					<LockType type={entry.params.type}/>
+				</td>
+			</tr>
+		)
+	};
+
+	renderer = (items, ref) => {
+		return (<table className={style.locklog}>
+			<thead>
+			<tr>
+				<th className={style.time}>Time</th>
+				<th className={style.event}>Event</th>
+				<th className={style.path}>Path</th>
+				<th className={style.type}>Type</th>
+			</tr>
+			</thead>
+			<tbody ref={ref}>
+			{items}
+			</tbody>
+		</table>);
+	};
+
+	render () {
+		return (
+			<ReactList
+				itemRenderer={this.renderRow}
+				itemsRenderer={this.renderer}
+				length={this.state.entries.length}
+				type='uniform'
+				/>
 		);
 	}
 }
